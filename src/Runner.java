fa83853c88e17068efa8e72ef698f75e4094db89
nicodiaz <nicodiaz@736bc674-2a1d-11de-b161-0f7b0ef88f65>
2009-04-16 08:41:01 +0000

    Funcionalidad: Revision General del Sistema Agregado: Eliminado: TODO: Revisar inpaintpixel. Los circulos creo que se estan recorriendo mal

--- src/Runner.java
+++ src/Runner.java
@@ -2,37 +2,42 @@ import image.ImageManipulator;
 import image.Mask;
 import image.YanInpainter;
 
-
-public class Runner {
+public class Runner
+{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		
+
 		if (args.length != 2)
 		{
-			System.out.println(" Falta argumentoees");
-			System.exit(2);
+			System.err
+					.println("Debe ingresar el nombre del archivo de imagen y el nombre del"
+							+ "archivo con la mascara");
+			System.err.println("Ejemplo: java Runner image.jpg mask.jpg");
+			System.exit(1);
 		}
-		
-		int[] whiteArray = {255, 255, 255};
-		int[] blackArray = {0, 0, 0};
-		
-		try{
-		ImageManipulator image = new ImageManipulator(args[0]);
-		Mask mask = new Mask(args[1], whiteArray, blackArray);
 
-		YanInpainter yaniImpaint = new YanInpainter(false);
-		yaniImpaint.inpaint(image, mask);
-		image.saveToFile("inpainteada1");
-		
-	
-//		yaniImpaint.setConsiderCurrentIteration(true);
-//		yaniImpaint.inpaint(image, mask);
-//		image.saveToFile("inpainteada2");
-		}catch (Exception e) {
+		int[] whiteArray = { 255, 255, 255 };
+		int[] blackArray = { 0, 0, 0 };
+
+		try
+		{
+			ImageManipulator image = new ImageManipulator(args[0]);
+			Mask mask = new Mask(args[1], whiteArray, blackArray);
+
+			YanInpainter yaniImpaint = new YanInpainter(false);
+			yaniImpaint.inpaint(image, mask);
+			image.saveToFile("outputIn.jpg");
+
+			// yaniImpaint.setConsiderCurrentIteration(true);
+			// yaniImpaint.inpaint(image, mask);
+			// image.saveToFile("inpainteada2");
+		} catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
