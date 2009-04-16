41b14c6b1156dd1d0341b091ed5965d403411ed3
msinia <msinia@736bc674-2a1d-11de-b161-0f7b0ef88f65>
2009-04-16 02:48:28 +0000

    Funcionalidad: Manipulacion de Imagen - Construccion y guardado Agregado: Construccion y escritura en disco Eliminado: ' TODO: implementar los demas metodos

--- src/image/ImageManipulator.java
+++ src/image/ImageManipulator.java
@@ -1,12 +1,25 @@
 package image;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class ImageManipulator {
 	private BufferedImage imgSrc = null;
 	
-	public ImageManipulator( String fileName ) {
-	
+	public ImageManipulator( String fileName ) throws IOException {
+		
+		// Preconditions
+		if (fileName == null)
+		{
+			throw new IllegalArgumentException();
+		}
+
+		imgSrc = ImageIO.read(new File(fileName));
+			
+		
 	}
 	
 	public ImageManipulator( BufferedImage imgSrc ) {
@@ -28,5 +41,23 @@ public class ImageManipulator {
 		return 0;
 	}
 	
+	public void saveToFile(String fileName) throws IOException
+	{
+		
+		// Preconditions
+		if (fileName == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		// Now we save the file
+		BufferedImage imgDst = new BufferedImage(imgSrc.getWidth(), imgSrc.getHeight(), 
+			BufferedImage.TYPE_INT_RGB);
+				
+		ImageIO.write(imgDst, "jpg", new File(fileName));
+		
+		
+		
+	}
 
 }
